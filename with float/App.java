c271986b778e670f363b02e19446ee2c15635ecc
Sean Kervin Vidal Gumarao <111546618+Nue1234@users.noreply.github.com>
2022-09-02 13:39:37 +0800

    Create App.java

---
+++ with float/App.java
@@ -0,0 +1,16 @@
+public class App {
+   public static void main(String[] args) {
+    
+    // My class
+    Myself sean = new Myself(50, 80, 90, 23, 168.8f, "Sean",true);
+
+    sean.sayMyName();
+    sean.sayMyAge();
+    sean.sayMyHeight();
+    sean.sayMyStrength();
+    sean.sayMyAgility();
+    sean.sayMyIntelligence();
+    sean.sayMyGender();
+    
+   }
+}
